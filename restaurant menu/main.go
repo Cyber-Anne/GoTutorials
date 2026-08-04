f31d7d77bc1f86d0b90b87cee9a817a09b0c40fa
Cyber-Anne <hayibordzifa9@gmail.com>
2026-08-04 20:40:09 +0000

    Added restaurant menu

---
+++ restaurant menu/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	burgerPrice := 35.00
+	pizzaPrice := 60.00
+	juicePrice := 12.00
+	cakePrice := 20.00
+
+	fmt.Print("Today's menu\n")
+	fmt.Print("\n")
+	fmt.Print("\n")
+	fmt.Printf("Burger..............%v\n", burgerPrice)
+	fmt.Printf("Pizza...............%v\n", pizzaPrice)
+	fmt.Printf("Juice...............%v\n", juicePrice)
+	fmt.Printf("Cake................%v\n", cakePrice)
+
+}
